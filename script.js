1d093259753df6e4bb81d6024d1e1df390119427
José Menor <josemenor@gmail.com>
2013-10-08 07:53:20 +0200

    generateForm now uses fragments to improve perfomance. Adds some CSS

--- script.js
+++ script.js
@@ -26,42 +26,91 @@ var allQuestions = [
     }
 ];
 
-function generateForm () {
+var score = 0;
+var myFieldset = document.getElementById('myFieldset');
 
-    var i = 0;
-    var currentQuestion = allQuestions[i];
-    var question = currentQuestion.question;
-    var choices = currentQuestion.choices;
-    var myFieldset = document.getElementById('myFieldset');
+var questionNumber = 0;
+
+//Sets the question and the answers
+var currentQuestion = allQuestions[questionNumber];
+var question = currentQuestion.question;
+var choices = currentQuestion.choices;
+var correctAnswer = currentQuestion.correctAnswer;
+
+
+
+
+function generateForm() {
+    
+    //Creates the fragment
     var myFragment = document.createDocumentFragment();
+    
+    //Creates the elements that'll go into the fragment
+    //First the question
     var hLegend = document.createElement('legend');
-    
     hLegend.textContent = question;
-    myFragment += hLegend;
-    myFieldset.appendChild(hLegend);
 
-    for (var j=0, len=choices.length ; j<len ; j+=1) {
+    //And appends it to the fragment
+    myFragment.appendChild(hLegend);
+
+    //We create a list to store the radios
+    var listContainer = document.createElement('ul');
+
+    //Then the possible answers
+    for (var i=0, len=choices.length ; i<len ; i+=1) {
     
-        var currentChoice = choices[j];
+        var currentChoice = choices[i];
 
-        var element = document.createElement('input');
+        //Creates a li element
+        var liTag = document.createElement('li');
+        
+        //Creates the input element
+        var option = document.createElement('input');
         //Assign different attributes to the element.
-        element.setAttribute('type','radio' );
-        element.setAttribute('value', currentChoice);
-        element.setAttribute('name', currentChoice);
+        option.setAttribute('type','radio' );
+        option.setAttribute('value', currentChoice);
+        option.setAttribute('name', 'userAnswer');
+        option.setAttribute('form', 'myForm');
 
         //Then the label
         var newLabel = document.createElement('label');
+        //the text that goes inside the label
         newLabel.appendChild(document.createTextNode(currentChoice));
     
-        myFieldset.appendChild(element);
-        myFieldset.appendChild(newLabel);
+        //Appends the created elements to the li tag
+        liTag.appendChild(option);
+        liTag.appendChild(newLabel);
 
+        //Appends the li tag to the ul
+        listContainer.appendChild(liTag);
     }
+
+    myFragment.appendChild(listContainer);
+
+
+
     
+    //The submit button
+    var submitButton = document.createElement('input');
+    submitButton.setAttribute('type','submit' );
+    submitButton.setAttribute('form', 'myForm');
+    submitButton.setAttribute('value','Let\'s go');
     
+    myFragment.appendChild(submitButton);
+
+    myFieldset.appendChild(myFragment);
+
 }
 
+//generates the first form
 generateForm();
+
+if (document.myForm.userAnswer[0].checked ) {
+    score += 10;
+}
+
+
+
+
 
 
